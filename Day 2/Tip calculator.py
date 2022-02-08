d0c4864df77d571d9e856db9eaa9300813c10e43
ionpostolache <ion_postolache00@yahoo.com>
2022-02-08 18:17:35 +0200

    updated

--- Day 2/Tip calculator.py
+++ Day 2/Tip calculator.py
@@ -19,3 +19,11 @@ How to limit a float to two decimal places in Python"""
 #Format the result to 2 decimal places = 33.60
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 #Write your code below this line 👇
+
+print("Welcome to the tip calculator!")
+bill=input("What was the total bill? ")
+tip=input("How much tip would you like to give? 10, 12, or 15? ")
+people=input("How many people to split the bill? ")
+tip_percent=(int(tip)+100)/100
+pay_each=(float(bill) / int(people)) * tip_percent
+print(pay_each)
