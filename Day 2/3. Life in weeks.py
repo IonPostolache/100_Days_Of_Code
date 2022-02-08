c229e4d7545c527c6bd3e5c216d147e59ea0bbe5
ionpostolache <ion_postolache00@yahoo.com>
2022-02-08 17:39:37 +0200

    updated

--- Day 2/3. Life in weeks.py
+++ Day 2/3. Life in weeks.py
@@ -23,3 +23,7 @@ age = input("What is your current age?")
 
 #Write your code below this line 👇
 
+months=(90-int(age))*12
+weeks=(90-int(age))*52
+days=(90-int(age))*365
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
