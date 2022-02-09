7ae3fb55b8b892bd487f7be1afc99d255ee9e618
ionpostolache <ion_postolache00@yahoo.com>
2022-02-09 11:28:11 +0200

    updated

--- Day 3/2. BMI 2.0.py
+++ Day 3/2. BMI 2.0.py
@@ -40,12 +40,13 @@ weight = float(input("enter your weight in kg: "))
 bmi=weight/height**2
 print(f"{int(weight)} ÷ ({height} x {height}) = {bmi}")
 round_bmi=int(round(bmi,0))
-print(f"Your BMI is {round_bmi}, you are slightly overweight.")
-
-"""
-Your BMI is 18, you are underweight."
-"Your BMI is 22, you have a normal weight."
-"Your BMI is 28, you are slightly overweight."
-"Your BMI is 33, you are obese."
-"Your BMI is 40, you are clinically obese."
-"""
+if round_bmi<=18.5:
+    print(f"Your BMI is {round_bmi}, you are underweight.")
+elif round_bmi<=25:
+    print(f"Your BMI is {round_bmi}, you have a normal weight.")
+elif round_bmi<=30:
+    print(f"Your BMI is {round_bmi}, you are slightly overweight.")
+elif round_bmi<=35:
+    print(f"Your BMI is {round_bmi}, you are obese.")
+else:
+    print(f"Your BMI is {round_bmi}, you are clinically obese.")
