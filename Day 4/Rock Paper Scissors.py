1cdbf22ab42ba5349e4a2ab4842a3b70d50c0f52
ionpostolache <ion_postolache00@yahoo.com>
2022-02-10 18:41:34 +0200

    updated

--- Day 4/Rock Paper Scissors.py
+++ Day 4/Rock Paper Scissors.py
@@ -37,21 +37,20 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-
 #Write your code below this line 👇
-options=["Rock", "Paper", "Scissors"]
+options=[rock, paper, scissors]
 human_choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-if human_choice<=0 or human_choice>2:
+
+if human_choice<0 or human_choice>2:
     print("You typed an invalid choice. You lost!")
 else:
     print(f"Your choice is {options[human_choice]}.")
+
     #generate a random choice for the computer
     computer_choice=random.randint(0,2)
     print(f"Computer choice is {options[computer_choice]}.")
-    #rules:
-    # Rock wins against scissors.
-    # Scissors win against paper.
-    # Paper wins against rock.
+
+    #determine the winner
     if human_choice==0 and computer_choice==2:
         print("You won!")
     elif human_choice==2 and computer_choice==0:
@@ -66,12 +65,3 @@ else:
         print("You lost!")
     elif human_choice==computer_choice:
         print("It's a draw!")
-
-####### Debugging challenge: #########
-#Try running this code and type 5.
-#It will give you an IndexError and point to line 32 as the issue.
-#But on line 38 we are trying to prevent a crash by detecting
-#any numbers great than or equal to 3 or less than 0.
-#So what's going on?
-#Can you debug the code and fix it?
-#Solution: https://repl.it/@appbrewery/rock-paper-scissors-debugged-end
