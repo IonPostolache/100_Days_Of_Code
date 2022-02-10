00e39c2ee0b0dd1c06c348c2a6f09947a7ff66b9
ionpostolache <ion_postolache00@yahoo.com>
2022-02-10 11:01:18 +0200

    updated

--- Day 4/Random Module.py
+++ Day 4/Random Module.py
@@ -1,10 +1,12 @@
 import random
 import my_module
-
+#print(my_module.pi)
 random_integer=random.randint(1,10)
 print(random_integer)
 
 random_float=random.random()
 print(random_float)
-
-#print(my_module.pi)
+modif=random_float*5
+print(modif)
+love_score=random.randint(1,100)
+print(f"Your love score is {love_score}.")
