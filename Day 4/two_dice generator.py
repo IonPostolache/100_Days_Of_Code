37ac2b546196e37267f53b32b60e81cca8bb6923
ionpostolache <ion_postolache00@yahoo.com>
2022-02-10 11:08:08 +0200

    updated

--- Day 4/two_dice generator.py
+++ Day 4/two_dice generator.py
@@ -2,4 +2,4 @@ import random
 
 dice1=random.randint(1,6)
 dice2=random.randint(1,6)
-print(f"Your pair of dice generated number is: {dice1} - {dice2}.")
+print(f"Your pair of dice generated number is: {dice1} - {dice2}")
