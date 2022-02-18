524cae9cf3c70b1c0396931372029aea4d7581cc
ionpostolache <ion_postolache00@yahoo.com>
2022-02-18 21:14:00 +0200

    updated

---
+++ Day 6/Hurdles challenge.py
@@ -0,0 +1,18 @@
+#this code is made to run on reeborg.ca/Hurdle 1
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for a in range(0, 6):
+    jump()
