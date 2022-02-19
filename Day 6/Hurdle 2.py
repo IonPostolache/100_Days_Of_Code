2fa57d740eee0d9680d57496624c427179117ad8
ionpostolache <ion_postolache00@yahoo.com>
2022-02-19 22:02:37 +0200

    updated

---
+++ Day 6/Hurdle 2.py
@@ -0,0 +1,23 @@
+#this code is made to run on reeborg.ca/Hurdle 2
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+#for a in range(0,6):
+    #jump()
+
+while not at_goal():
+    jump()
