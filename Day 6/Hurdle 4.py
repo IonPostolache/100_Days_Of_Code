95a87e7938103bd0e63c3ac2946f9c8ec43044c7
ionpostolache <ion_postolache00@yahoo.com>
2022-02-20 10:18:43 +0200

    updated

---
+++ Day 6/Hurdle 4.py
@@ -0,0 +1,27 @@
+#this code is made to run on reeborg.ca/Hurdle 3
+http://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while not right_is_clear():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+def go():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+while not at_goal():
+    go()
