a2722a314d2bc297df8a7b9ddcbebebee70fef22
ionpostolache <ion_postolache00@yahoo.com>
2022-02-22 20:13:43 +0200

    initial sketch

---
+++ Day 7/Hangman_initial_sketch.py
@@ -0,0 +1,15 @@
+#import random
+#set initial hangman picture variable
+#set initial word variable
+#def input--asking user for a letter
+#if letter in word:
+#display the new word
+#elif not in word:
+#add symbol in hangman picture
+#end of funtion
+#while word not complete and hungman not complete:
+#call the function again
+#if word is complete or hangman is complete: END
+#
+#
+#
