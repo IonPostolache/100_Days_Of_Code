1ac95bd97c236ca66e6e238a26b80331f9dcbe24
ionpostolache <ion_postolache00@yahoo.com>
2022-03-01 08:10:26 +0200

    update

--- Day 7/Hangman 2.py
+++ Day 7/Hangman 2.py
@@ -16,24 +16,15 @@ guess = input("Guess a letter: ").lower()
 display=[]
 for ch_let in chosen_word:
     display.append("_")
+#print(display)
 
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-display_new=[]
-for item in chosen_word:
-    if item==guess:
-        display_new=display.replace("_", guess)
-print(display)
-print(display_new)
-
-
-
-#strings = ["a", "ab", "aa", "c"]
-#new_strings = []
-#for string in strings:
-#new_string = string. replace("a", "1") Modify old string.
+for i in range(len(chosen_word)):
+    if chosen_word[i] == guess:
+        display[i] = guess
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
