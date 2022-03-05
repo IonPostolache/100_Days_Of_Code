2f5949bf2681d83cf77ca3da516376fa799c0338
ionpostolache <ion_postolache00@yahoo.com>
2022-03-05 12:14:32 +0200

    update

--- Day 8/1. greet exercise.py
+++ Day 8/1. greet exercise.py
@@ -2,11 +2,22 @@
 # Create a function called greet().
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
-
+"""
 def greet():
     print("statement 1")
     print("statement 2")
     print("statement 3")
+greet()"""
 
-greet()
+"""
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"How is it to be in {location}?")
     
+greet_with("Jack", "Paris")"""
+
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"How is it to be in {location}?")
+
+greet_with(location="Paris", name="Jack B")
