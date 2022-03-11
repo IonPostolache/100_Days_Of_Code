d38b7c629f59f3f521886e709636d282b851b96f
ionpostolache <ion_postolache00@yahoo.com>
2022-03-11 19:53:18 +0200

    updated

--- Day 9/2. Nesting dictionary.py
+++ Day 9/2. Nesting dictionary.py
@@ -1,3 +1,4 @@
+#Nesting a Dictionary inside a Dictionary
 travel_log={
     "France":{"cities_visited":["Paris", "Lille", "Dijon"], "total_visits":12},
     "Germany": {"cities_visited":["Berlin", "Hamburg", "Stuttgart"], "total_visits":5}
@@ -5,4 +6,10 @@ travel_log={
 
 print(travel_log)
 
-#key=cities_visited
+
+travel_log=[
+    {"country":"France","cities_visited":["Paris", "Lille", "Dijon"], "total_visits":12},
+     {"country":"Germany", "cities_visited":["Berlin", "Hamburg", "Stuttgart"], "total_visits":5}
+]
+
+print(travel_log)
