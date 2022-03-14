c78601b1574efbf0adb483cebed60e167ab78c8c
ionpostolache <ion_postolache00@yahoo.com>
2022-03-14 21:48:22 +0200

    updated

--- Day 9/4. Secret Auction Program.py
+++ Day 9/4. Secret Auction Program.py
@@ -1,5 +1,6 @@
 #from replit import clear
 #HINT: You can call clear() to clear the output in the console.
+#replit only works on replit website?
 
 from art import logo
 print(logo)
@@ -12,23 +13,13 @@ while flag==True:
     bidder_name=input("What is your name?")
     bid_price=input("What is your bid price?")
     all_bidders_dict[bidder_name]=bid_price
-    print(all_bidders_dict)
+    #print(all_bidders_dict)
 
     more_users=input("Are there other users who want to bid? 'yes' or 'no'.")
     if more_users=="yes":
-        #clear the screen
         #clear()
         continue
     elif more_users=="no":
-        #find the highest bid in the dictionary and declare them as a winner.
-        for key, value in all_bidders_dict.items():
-            #if int(value)>int(counter):
-            if int(all_bidders_dict[key])>counter:
-                counter=all_bidders_dict[value]
-
-                #counter=value
-        print(value[counter])
-        print(f"The winner is: {counter}")
+        max_key=max(all_bidders_dict, key=all_bidders_dict.get)
+        print(f"The winner is: {max_key}")
         flag=False
-
-
