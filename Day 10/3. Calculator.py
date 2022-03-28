e5efba123d18ffe6f20261147999b9b9bb86a4f4
ionpostolache <ion_postolache00@yahoo.com>
2022-03-28 20:31:18 +0300

    updated

--- Day 10/3. Calculator.py
+++ Day 10/3. Calculator.py
@@ -13,30 +13,24 @@ def divide(n1,n2):
 
 operations={"+":add,"-":subtract, "*":multiply, "/":divide}
 
-num1=int(input("What is the first number? "))
+def calculation():
+    num1=int(input("What is the first number? "))
+    for k in operations.keys():
+        print(k)
 
+    continuation="y"
+    while continuation!="n":
+        operation_symbol=input("Pick an operation: ")
+        num3=int(input("What is the next number? "))
+        answer_temp=operations[operation_symbol]
+        second_answer=answer_temp(num1,num3)
+        print(f"{num1} {operation_symbol} {num3} = {second_answer}")
+        continuation = input(f"Type 'y' to continue calculating with {second_answer}, or type 'n' to exit.")
+        num1=second_answer
+        if continuation=="n":
+            calculation()
 
-
-for k in operations.keys():
-    print(k)
-operation_symbol=input("Pick an operation from the line above: ")
-num2=int(input("What is the next number? "))
-
-answer_temp=operations[operation_symbol]
-first_answer=answer_temp(num1,num2)
-
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-
-continuation = input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to exit.")
-
-while continuation!="n":
-    operation_symbol=input("Pick another operation: ")
-    num3=int(input("What is the next number? "))
-    answer_temp=operations[operation_symbol]
-    second_answer=answer_temp(first_answer,num3)
-    print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
-    continuation = input(f"Type 'y' to continue calculating with {second_answer}, or type 'n' to exit.")
-    first_answer=second_answer
+calculation()
 
 
 
