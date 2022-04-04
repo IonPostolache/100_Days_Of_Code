ea64729179574c814f07baa51b91edc3514ec005
ionpostolache <ion_postolache00@yahoo.com>
2022-04-04 07:47:18 +0300

    it's working

--- Day 13/1.  Debugging.py
+++ Day 13/1.  Debugging.py
@@ -32,24 +32,24 @@ if age > 18:
     print(f"You can drive at age {age}.")"""
 
 
-#Print is Your Friend
+"""#Print is Your Friend
 pages = 0
-print(pages)
+#print(pages)
 word_per_page = 0
 pages = int(input("Number of pages: "))
-print(pages)
+#print(pages)
 #word_per_page == int(input("Number of words per page: ")) #wrong code
 word_per_page = int(input("Number of words per page: "))
-print(word_per_page)
+#print(word_per_page)
 total_words = pages * word_per_page
-print(total_words)
+print(total_words)"""
 
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
+#Use a Debugger
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
 
-# mutate([1,2,3,5,8,13])
+mutate([1,2,3,5,8,13])
