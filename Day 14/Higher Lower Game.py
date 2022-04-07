8d87a7b9fb46be6525d32c76e1b5ccdc8930a1a3
IonPostolache <ion_postolache00@yahoo.com>
2022-04-07 08:52:42 +0300

    the code is working

--- Day 14/Higher Lower Game.py
+++ Day 14/Higher Lower Game.py
@@ -10,35 +10,37 @@ def random_generator():
     return generated
 
 
-ran_A=random_generator()
-ran_B=random_generator()
+ran_a = random_generator()
+ran_b = random_generator()
+
+flag = True
 
 
-flag=True
 def play():
     """function to play Higher Lower game"""
     print(logo)
-    new_score=0
-    temp_ran_A=ran_A
-    while flag==True:
-        ran_B=random_generator()
-        print(f"Compare A: {data[temp_ran_A]['name']}, a {data[temp_ran_A]['description']}, from {data[temp_ran_A]['country']}.")
+    new_score = 0
+    t_ran_a = ran_a
+    while flag:
+        ran_b2 = random_generator()
+        print(
+            f"Compare A: {data[t_ran_a]['name']}, a {data[t_ran_a]['description']}, from {data[t_ran_a]['country']}.")
         print(vs)
-        print(f"Against B: {data[ran_B]['name']}, a {data[ran_B]['description']}, from {data[ran_B]['country']}.")
-        selection=input("Who has more followers? Type 'A' or 'B': ")
+        print(f"Against B: {data[ran_b2]['name']}, a {data[ran_b2]['description']}, from {data[ran_b2]['country']}.")
+        selection = input("Who has more followers? Type 'A' or 'B': ")
         print(logo)
-        if (selection == "A" and data[temp_ran_A]['follower_count'] > data[ran_B]['follower_count']) or \
-                (selection == "B" and data[temp_ran_A]['follower_count'] < data[ran_B]['follower_count']):
+        if (selection == "A" and data[t_ran_a]['follower_count'] > data[ran_b2]['follower_count']) or \
+                (selection == "B" and data[t_ran_a]['follower_count'] < data[ran_b2]['follower_count']):
             new_score += 1
             print(f"You're right! Current score: {new_score}.")
-        elif (selection == "A" and data[temp_ran_A]['follower_count'] < data[ran_B]['follower_count']) or \
-                (selection == "B" and data[temp_ran_A]['follower_count'] > data[ran_B]['follower_count']):
+        elif (selection == "A" and data[t_ran_a]['follower_count'] < data[ran_b2]['follower_count']) or \
+                (selection == "B" and data[t_ran_a]['follower_count'] > data[ran_b2]['follower_count']):
             new_score -= 1
-            if new_score<0:
-                new_score=0
+            if new_score < 0:
+                new_score = 0
             print(f"Sorry, that's wrong. Final score: {new_score}")
             break
-        temp_ran_A=ran_B
+        t_ran_a = ran_b2
+
 
 play()
-
