5daa6cc179feab3ddd8219ead44f20f1fe7cfd3e
IonPostolache <ion_postolache00@yahoo.com>
2022-06-21 18:51:02 +0300

    OOP turtle move

---
+++ Day 16/1. OOP start.py
@@ -0,0 +1,14 @@
+#import turtle
+from turtle import Turtle, Screen
+
+#timmy=turtle.Turtle()
+tomitza=Turtle()
+print(tomitza)
+tomitza.shape("turtle")
+tomitza.color("darkred")
+tomitza.forward(100)
+
+my_screen=Screen()
+print(my_screen.canvheight)
+#turtle.color("Darkred")
+my_screen.exitonclick()
