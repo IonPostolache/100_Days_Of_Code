68cbd6c25d5350e74ef129b5767b954a69f1dc88
IonPostolache <ion_postolache00@yahoo.com>
2023-03-11 17:14:03 +0200

    table added

--- Day 16/1. OOP start.py
+++ Day 16/1. OOP start.py
@@ -1,14 +1,32 @@
-#import turtle
+# import turtle
 from turtle import Turtle, Screen
 
-#timmy=turtle.Turtle()
-tomitza=Turtle()
+# timmy=turtle.Turtle()
+tomitza = Turtle()
 print(tomitza)
 tomitza.shape("turtle")
-tomitza.color("darkred")
-tomitza.forward(100)
+tomitza.color("coral")
+tomitza.forward(200)
+tomitza.back(100)
 
-my_screen=Screen()
+my_screen = Screen()
 print(my_screen.canvheight)
-#turtle.color("Darkred")
+# turtle.color("Darkred")
 my_screen.exitonclick()
+
+# import prettytable
+from prettytable import PrettyTable
+
+table = PrettyTable()
+# table.add_column(fieldname="name", column=1)
+# print(table)
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"], align="l")
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+table.min_table_width = 60
+print(table)
+
+
+
+
