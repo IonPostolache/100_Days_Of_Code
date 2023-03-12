c6724c18bd3ea2fe6c198f27b05c15da6bb37565
IonPostolache <ion_postolache00@yahoo.com>
2023-03-12 09:33:07 +0200

    currently processing coins

--- Day 16/coffee_maker.py
+++ Day 16/coffee_maker.py
@@ -22,15 +22,6 @@ class CoffeeMaker:
                 can_make = False
         return can_make
 
-    # def is_resource_sufficient(self, drink):
-    #     """Returns True when order can be made, False if ingredients are insufficient."""
-    #     can_make = True
-    #     for item in drink.ingredients:
-    #         if drink.ingredients[item] > self.resources[item]:
-    #             print(f"Sorry there is not enough {item}.")
-    #             can_make = False
-    #     return can_make
-
     def make_coffee(self, order):
         """Deducts the required ingredients from the resources."""
         for item in order.ingredients:
--- Day 16/main.py
+++ Day 16/main.py
@@ -2,40 +2,41 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-menu_items = Menu()
-options = menu_items.get_items()
+turning_off_message = "off"
+report_message = "report"
+
+menu = Menu()
+available_menu_items = menu.get_items()
+drinks = available_menu_items.split("/")[0:-1]
+# print(drinks)
+
+coffe_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu_item = MenuItem()
 
 
 def users_prompt():
-    users_response = input(f"What would you like? ({options}):")
+    users_response = input(f"What would you like? ({available_menu_items}):")
     return users_response
 
 
-response = users_prompt()
-turn_off_message = "off"
-coffe = CoffeeMaker()
-drinks = options.split("/")[0:-1]
-print(drinks)
-
-while response != turn_off_message:
-    if response == "report":
-        coffe.report()
-    elif response in drinks:
-        print("yes, response in drinks")
-
-        # coffe.is_resource_sufficient(response)
-        # can_make = CoffeeMaker().is_resource_sufficient(response)
-        menu2 = MenuItem()
-        can_make2 = menu2.ingredients
-        can_make = coffe.is_resource_sufficient(response)
-        if can_make:
-            print("can_make = True")
-    # else:
-    #     print("no")
+def main_function():
     response = users_prompt()
+    while response != turning_off_message:
+        if response == report_message:
+            coffe_maker.report()
+        elif response in drinks:
+            # print("yes, response in drinks")
+            drink = menu.find_drink(response)
+            # print(f"found drink: {drink.name}")
+            if coffe_maker.is_resource_sufficient(drink):
+                money_machine.make_payment(menu_item.cost)
+
+                coffe_maker.make_coffee(drink)
+
+        response = users_prompt()
+    print("Turning Off the Coffee Machine...")
 
 
+main_function()
 
-
-
-print("Turning Off the Coffee Machine...")
